9d21b785a4db7f96d1ee0c2f1113adcb2c45e60a
SatyamguptaSG <105717874+SatyamguptaSG@users.noreply.github.com>
2022-06-16 17:23:13 +0530

    Update Cuckoo_hashing.v

--- Cuckoo_hashing.v
+++ Cuckoo_hashing.v
@@ -1,25 +1,27 @@
-module f1(input [31:0]num,output [4:0]i1)
+`include "cuckoo_tb.v"
+module f1(input [31:0]num,output [4:0]i1);
+reg [4:0]i1;
 integer i=num;
-for(;i>=10;i=i-10){
-}
+for(i=num;i>=10;i=i-10)begin
+end
 assign i1=i;
-endmodule;
+endmodule
 
-module f2(input [31:0]num,output [4:0]i2)
+module f2(input [31:0]num,output [4:0]i2);
 integer i=num*3;
-for(;i>=10;i=i-10){
-}
+for(i=num*3;i>=10;i=i-10)begin
+end
 assign i2=i;
-endmodule;
+endmodule
 
-module arr()
+module arr();
 reg [31:0]arr1[9:0];
 reg [31:0]arr2[9:0];
 reg ch1[9:0];//stores if number is stored in arr1
 reg ch2[9:0];//stores if number is stored in arr2
-endmodule;
+endmodule
 
-module insert(input [31:0]num,output pos)// pos i position in idex
+module insert(input [31:0]num,output pos);// pos i position in idex
 wire [31:0] dummy1;
 wire [31:0] dummy2;
 // f1(num,i1);
@@ -31,40 +33,48 @@ dummy->f2->index->array->
 */
 integer count=1;
 assign dummy1=num;
-always @(count<=20)begin
-    if(~flag)begin
-        f1(dummy1,i1)
-        if(ch1[i1]==1){
-            assign dummy2=arr[i1];
-            assign arr1[i1]=dummy1;
-            ch1[i1]=1;
-            flag=1;
-            count=count+1;
-        }
-        else{
-            arr[i1]=dummy1;
-            count=count+1;
-            ch1[i1]=1;
-        }
-    end 
-    else(flag) begin
-        f2(dummy2,i2)
-        if(ch2[i2]==1){
-            assign dummy1=arr2[i2];
-            assign arr[i2]=dummy2;
-            ch2[i2]=1;
-            flag=0;
-            count=count+1;
-        }
-        else{
-            arr[i2]=dummy2;
-            count=count+1;
-            ch1[i2]=1;
-        }
-    end;
-end;
-endmodule; 
+f1 five(dummy1,i1);
+assign pos=i1;
+always() begin
+    for(count=1;count<=20;count=count+1)begin
+        if(~flag)begin
+            f1(dummy1,i1);
+            if(ch1[i1]==1)begin
+                assign dummy2=arr[i1];
+                assign arr1[i1]=dummy1;
+                assign ch1[i1]=1;
+                assign flag=1;
+            end
+            else begin
+                assign arr[i1]=dummy1;
+                assign count=count+1;
+                assign ch1[i1]=1;
+            end
+        end 
+        else begin
+            f2(dummy2,i2);
+            if(ch2[i2]==1)begin
+                assign dummy1=arr2[i2];
+                assign arr[i2]=dummy2;
+                assign ch2[i2]=1;
+                assign flag=0;
+            end
+            else begin
+                assign arr[i2]=dummy2;
+                assign count=count+1;
+                assign ch1[i2]=1;
+            end
+        end
+    end
+end
+endmodule 
 
-module search(input [31:0]num)
+module search(input [31:0]num);
 /*number ka f1 f2 */
-endmodule;
+endmodule
+module insert_tb;
+reg[31:0] num,pos;
+insert add(num,pos);
+assign num=31'd42;
+endmodule
+
